2ac6526de4efdd92a0599a6fb2140834c4fec729
阿偉特 <aa4192696@gmail.com>
2023-12-28 14:15:15 +0800

    update

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.dao.UserDao;
+import com.example.entiry.User;
 
 /**
  * http://localhost:8080/ThesisProjec/mvc/user/all
@@ -20,9 +24,16 @@ public class UserController {
 	@Qualifier("userDaoImpl")
 	UserDao userDaoImpl;
 	
+//	@GetMapping("/all")
+//	@ResponseBody
+//	public String getAllUser() {
+//		return userDaoImpl.findAllUser().toString();
+//	}
+	
 	@GetMapping("/all")
-	@ResponseBody
-	public String getAllUser() {
-		return userDaoImpl.findAllUser().toString();
+	public String getAllUser(Model model) {
+		List<User> users = userDaoImpl.findAllUser();
+		model.addAttribute("users", users);
+		return "user_list";
 	}
 }
