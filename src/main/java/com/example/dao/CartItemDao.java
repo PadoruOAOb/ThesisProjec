d70da0c730514c5e101ffefc7a31d6f9e1ef9dc1
PadoruOAOb <tommy60512@gmail.com>
2024-01-19 23:33:44 +0800

    update

--- src/main/java/com/example/dao/CartItemDao.java
+++ src/main/java/com/example/dao/CartItemDao.java
@@ -7,4 +7,21 @@ import com.example.entiry.CartItem;
 public interface CartItemDao {
 
 	List<CartItem> findAllCartItems();
+
+	void addCartItem(CartItem cartItem);
+
+	void updateCartItem(CartItem cartItem);
+
+	void deleteCartItem(int cartItemId);
+
+    CartItem getCartItemById(Integer cartItemId);
+
+    // 獲取特定購物車中的特定課程項目
+    CartItem getCartItemByCourse(Integer cartId, Integer courseId);
+
+    // 更新特定購物車中特定課程的數量
+    void updateCartItemQuantity(Integer cartItemId, Integer newQuantity);
+
+    // 刪除特定購物車中特定課程的項目
+    void deleteCartItemByCourse(Integer cartId, Integer courseId);
 }
