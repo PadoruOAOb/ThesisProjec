b9c36ac002a748ff5bc8aaa158f9ec7c740bd7a6
祥 <@DESKTOP-UV49I6C>
2024-01-22 01:01:31 +0800

    upup

--- src/main/java/com/example/dao/CourseDao.java
+++ src/main/java/com/example/dao/CourseDao.java
@@ -1,5 +1,22 @@
 package com.example.dao;
 
-public class CourseDao {
+import java.util.List;
 
+import com.example.entiry.Course;
+
+public interface CourseDao {
+	// 根據課程ID查詢課程
+    Course getCourseById(int courseId);
+
+    // 查詢所有課程
+    List<Course> getAllCourses();
+
+    // 添加新課程
+    void addCourse(Course course);
+
+    // 更新課程信息
+    void updateCourse(Course course);
+
+    // 刪除課程
+    void deleteCourse(int courseId);
 }
--- src/main/java/com/example/dao/CourseDaoImpl.java
+++ src/main/java/com/example/dao/CourseDaoImpl.java
@@ -1,5 +1,66 @@
 package com.example.dao;
 
-public interface CourseDaoImpl {
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.example.entiry.Course;
+import com.example.entiry.User;
+
+
+/**
+ courseId,courseName,price,courseDescription,
+ courseDetail,courseImg,teacher,typeId,createTime;
+
+ */
+@Repository
+public class CourseDaoImpl implements CourseDao {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	RowMapper<Course> rowMapper = (ResultSet rs, int rowNum) -> {
+		Course course = new Course();
+		course.setcourseId(rs.getInt("courseId"));
+		course.setcourseName(rs.getString("courseName"));
+		course.setprice(rs.getString("price"));
+		course.setcourseDescription(rs.getString("courseDescription"));
+		course.setteacher(rs.getInt("teacher"));
+		course.setcourseDetail(rs.getString("courseDetail"));
+		course.setcourseImg(rs.getString("courseImg"));
+		course.settypeId(rs.getInt("typeId"));
+		course.createTime(rs.getTimestamp("createTime"));
+		return course;
+	};
+	@Override
+	public Course getCourseById(int courseId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Course> getAllCourses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void addCourse(Course course) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void updateCourse(Course course) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void deleteCourse(int courseId) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
